5e4a14b6da6fbbb7490a9040ecf39ec5878449c0
Rohit Bagal <bagalrohit75@gmail.com>
2023-06-16 07:24:08 +0530

    b14

--- src/App.js
+++ src/App.js
@@ -3,74 +3,37 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <>
-      <h1>My Project</h1>
-      <ListDemo />
+      <h1>My Todo</h1>
+      <MyTodo />
     </>
   );
 }
 
-function ListDemo() {
+function MyTodo() {
   let inputRef = useRef();
-  let [list, setList] = useState(["delhi"]);
+  let [todo, setTodo] = useState({Task:""});
 
-  let addItemAction = () => {
-    let inputValue = inputRef.current.value;
-    let newList = [inputValue, ...list];
-    setList(newList);
+  let handlerChangeTaskAction = (e) => {
+    console.log(e.target);
+    let newTodo = {...todo,task:e.target.value};
+    setTodo(newTodo);
 
-    inputRef.current.value = "";
+  };
+  let addTodoAction = () => {
+    alert(todo.task);
   };
 
   return (
     <>
-      <div
-        className="row justify-content-center align-items-center"
-        style={{ height: "100vh" }}
-      >
-        <div className="col-sm-12 col-md-6">
-          <h1>Registraton App</h1>
           <input
             className="form-control"
             type="text"
-            id="id1"
-            ref={inputRef}
-            placeholder="Enter user input..."
+            placeholder="Enter Task"
+            value={todo.task}
+            onChange={handlerChangeTaskAction}
           />
-          <input
-            className="form-control"
-            type="text"
-            id="id1"
-            ref={inputRef}
-            placeholder="Enter Password..."
-          />
-          <input
-            className="form-control"
-            type="text"
-            id="id1"
-            ref={inputRef}
-            placeholder="Enter Email..."
-          />
-          <input type="button" value="Login" onClick={addItemAction} />
-        </div>
-      </div>
-    </>
-  );
-}
-
-function MessageDemo({ message }) {
-  return (
-    <>
-      <h1>Hello {message}</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-        dolorem quas sequi tenetur quo odit cupiditate assumenda voluptas
-        quibusdam blanditiis voluptatem quis mollitia eaque molestiae animi,
-        provident eligendi facilis incidunt.
-      </p>
-      <div>
-        <input type="button" value="&#128077;" />
-        <input type="button" value="&#128078;" />
-      </div>
+          <input type="button" value="Add Todo" onClick={addTodoAction} />
+       
     </>
   );
 }
