a2d2e4eed757924cd496f017dfffb7ae8540f9c7
Rohit Bagal <bagalrohit75@gmail.com>
2023-06-16 15:47:23 +0530

    b22

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Link to={"/home"}>Home |</Link>
-        <Link to={"/todo"}>Todo |</Link>
-        <Link to={"/registration"}>Registration</Link>
+       
+      <MyNavigationLinks />
 
         <Routes>
           {/** 1 Route means 1 Page */}
           <Route path="/" element={<h1>Hello</h1>} />
           <Route path="/home" element={<h1>Home</h1>} />
           <Route path="/todo" element={<MyTodo />} />
-          <Route path="/registration" element={<MyRegistration />} />
+        <Route path="/registration" element={<MyRegistration />} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
+        <MyNavigationLinks />
+
       </BrowserRouter>
     </>
   );
 }
-
+function MyNavigationLinks(){
+  return(
+    <>
+<Link to={"/home"}>Home |</Link>
+<Link to={"/todo"}>Todo |</Link>
+<Link to={"/registration"}>Registration</Link>
+export default App;
+</>
+  );
+}
 export default App;
