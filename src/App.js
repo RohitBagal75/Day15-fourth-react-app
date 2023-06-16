8382d2479dfdba98d5326970b879b3bbe419fbc1
Rohit Bagal <bagalrohit75@gmail.com>
2023-06-16 15:31:08 +0530

    b20

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
-import { useRef, useState } from "react";
-import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyTodo from "./pages/MyTodo";
+import MyRegistration from "./pages/MyRegistration";
+
 function App() {
   return (
     <>
-      <h1>My Todo</h1>
-      <MyTodo />
-    </>
-  );
-}
-
-function MyTodo() {
-  
-  let [todo, setTodo] = useState({Task:"",description:""});
-
-  let handlerChangeTaskAction = (e) => {
-    console.log(e.target);
-    let newTodo = {...todo,task:e.target.value};
-    setTodo(newTodo);
-
-  };
-  
-  let handlerChangeDescriptionAction = (e) => {
-    console.log(e.target);
-    let newTodo = {...todo,description:e.target.value};
-    setTodo(newTodo);
-
-  };
-  let addTodoAction = () => {
-   console.log(todo);
-  };
-
-  return (
-    <>
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Enter Task"
-            value={todo.task}
-            onChange={handlerChangeTaskAction}
-          />
-          <textarea
-            className="form-control"
-            col="30"
-            rows="3"
-            placeholder="Enter Description"
-            value={todo.description}
-            onChange={handlerChangeDescriptionAction}
-          ></textarea>
-          <input type="button" value="Add Todo" onClick={addTodoAction} />
-       
+      <BrowserRouter>
+     
+        <Routes>
+          {/** 1 Route means 1 Page */}
+          <Route path="/" element={<h1>Hello</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/todo" element={<MyTodo />} />
+          <Route path="/registration" element={<MyRegistration />} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
