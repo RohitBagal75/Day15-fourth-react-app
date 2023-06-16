fa95b76a8a314dcfe7c76d0c1f65d02460ae4091
Rohit Bagal <bagalrohit75@gmail.com>
2023-06-16 18:15:14 +0530

    b25

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
-
-import { BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 import MyNavigationLinks from "./pages/MyNavigationLinks";
+import MyNavigationLinksUsingButton from "./pages/MyNavigationLinksUsingButton";
+
 function App() {
   return (
     <>
       <BrowserRouter>
-       
-      <MyNavigationLinks />
+        <MyNavigationLinks />
 
         <Routes>
           {/** 1 Route means 1 Page */}
           <Route path="/" element={<h1>Hello</h1>} />
           <Route path="/home" element={<h1>Home</h1>} />
           <Route path="/todo" element={<MyTodo />} />
-        <Route path="/registration" element={<MyRegistration />} />
+          <Route path="/registration" element={<MyRegistration />} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
-        <MyNavigationLinksUsingButton />
 
+        <MyNavigationLinksUsingButton />
       </BrowserRouter>
     </>
-  
   );
 }
-function MyNavigationLinksUsingButton(){
-   const navigate = useNavigate();
 
-   let goToHomeAction = () => {
-    navigate("./Home");
-   };
-   let goToTodoAction = () => {
-    navigate("./todo");
-   };
-   let goToRegistrationAction = () => {
-    navigate("./registration");
-   };
-
-   return (
-    <div>
-      <input type="button" value="Home" onClick={goToHomeAction} />
-      <input type="button" value="Todo" onClick={goToTodoAction} />
-      <input type="button" value="Registartion" onClick={goToRegistrationAction}/>
-    </div>
-   );
-}
 export default App;
